2c153885404e645a11f79f7f9ec028bde96eab83
Simon Kimotho <kimothokarani@gmail.com>
2025-07-06 21:38:59 +0300

    Initial commit (no secrets)

---
+++ listings/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+from rest_framework import generics
+from .models import Listing
+from .serializers import ListingSerializer
+
+class ListingListCreateView(generics.ListCreateAPIView):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
